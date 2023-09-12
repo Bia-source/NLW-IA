3ce32cfcc5fda0c5306341d33cf6f4908a2ccf24
Bia-source <bia_ferreirads@yahoo.com>
2023-09-12 18:07:13 -0300

    input de upload do video inserido

--- upload-ai-web/src/App.tsx
+++ upload-ai-web/src/App.tsx
@@ -1,7 +1,8 @@
 import { Button } from "./components/ui/button";
-import { Github } from "lucide-react";
+import { Github, FileVideo } from "lucide-react";
 import { Separator } from "./components/ui/separator";
 import { Textarea } from "./components/ui/textarea";
+import { Label } from "./components/ui/label";
 
 export function App() {
   return (
@@ -28,10 +29,30 @@ export function App() {
             </div>
 
             <p className="text-sm text-muted-foreground"> 
-              Lembre-se: você pode utilizar a variável <code className="text-orange-400">{'{transcription}'}</code> no seu prompt para adicionar o conteúdo da transcricao do vídeo selecionado.
+              Lembre-se: você pode utilizar a variável <code className="text-orange-400">{'{transcription}'}</code> no seu prompt para adicionar o conteúdo da transcrição do vídeo selecionado.
             </p>
         </div>
-        <aside className="w-80"></aside>
+        <aside className="w-80 space-y-6">
+            <form className="space-y-6">
+                <label htmlFor="video" className="border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-white/5">
+                  <FileVideo className="w-4 h-4"/>
+                  Selecione um video
+                </label>
+
+                <input type="file" id="video" accept="video/mp4" className="sr-only"/>
+
+                <Separator/>
+
+                <div className="space-y-1">
+                  <Label htmlFor="transcription_prompt">Prompt de transcrição</Label>
+                  <Textarea id="transcription_prompt" className="min-h-20 leading-relaxed" placeholder="Inclua palavras-chave mencionadas no vídeo separadas por virgula (,)"/>
+                </div>
+
+                <Button>
+                  Carregar
+                </Button>
+            </form>
+        </aside>
       </main>
     </div>
   )
