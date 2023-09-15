cbdbeec32d43f0abf10ccfdf03f6ea347065456a
Bia-source <bia_ferreirads@yahoo.com>
2023-09-14 21:02:31 -0300

    gerando resposta da AI

--- upload-ai-api/src/server.ts
+++ upload-ai-api/src/server.ts
@@ -2,12 +2,14 @@ import { fastify } from "fastify";
 import { getAllPromptsRoute } from "./routes/getAllPrompts.routes";
 import { uploadVideoRoute } from "./routes/uploadVideo.routes";
 import { createTranscriptionRoute } from "./routes/createTranscription.routes";
+import { generationAIcompletionRoute } from "./routes/generateAIcompletion.routes";
 
 const app = fastify();
 
 app.register(getAllPromptsRoute);
 app.register(uploadVideoRoute);
 app.register(createTranscriptionRoute);
+app.register(generationAIcompletionRoute);
 
 app.listen({
     port: 3333
